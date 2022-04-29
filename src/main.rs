5dca8ee5cc96451d38bc363d8f6efd48979b49e3
SovietUfo <ommm@interia.pl>
2022-04-29 17:52:12 +0200

    structs and consts

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use structopt::StructOpt;
+
+
+const KIOCSOUND: u64 = 0x4B2F;
+// genuinely no idea where this comes from but it makes things work
+const CLOCK_TICK_RATE: u32 = 1193180;
+
+#[derive(StructOpt, Debug)]
+struct Opt {
+    /// frequency of the beep (in hz)
+    #[structopt(short, long)]
+    freq: u16,
+    #[structopt(short, long)]
+    /// how long should the beep be (in ms)
+    time: u32
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    
 }
