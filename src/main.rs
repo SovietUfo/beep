382ad4f0f581eef737abde3b836aa8bb75fd1161
SovietUfo <ommm@interia.pl>
2022-04-30 14:00:29 +0200

    check for user is root

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::{fs::OpenOptions, os::unix::prelude::AsRawFd, thread, time::Duration};
-use nix::ioctl_write_int_bad;
+use nix::{ioctl_write_int_bad, unistd};
 use structopt::StructOpt;
 
 
@@ -9,11 +9,11 @@ const CLOCK_TICK_RATE: u32 = 1193180;
 
 #[derive(StructOpt, Debug)]
 struct Opt {
-    /// frequency of the beep (in hz)
+    /// frequency of the beep (in hz).
     #[structopt(short, long)]
     freq: u16,
     #[structopt(short, long)]
-    /// how long should the beep be (in ms)
+    /// how long should the beep be (in ms).
     time: u32
 }
 
@@ -22,7 +22,11 @@ ioctl_write_int_bad!(kiocsound, KIOCSOUND);
 fn main() {
     let args = Opt::from_args();
 
-    let device = OpenOptions::new().append(true).open("/dev/console").expect("couldn't open /dev/console");
+    if !unistd::getuid().is_root() {
+        eprintln!("root privileges are required.");
+        return
+    }
+    let device = OpenOptions::new().append(true).open("/dev/console").expect("couldn't open /dev/console.");
     let period = CLOCK_TICK_RATE.checked_div(args.freq as u32).unwrap_or(0);
 
     unsafe { kiocsound(device.as_raw_fd(), period as i32).unwrap(); }
